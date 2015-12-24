1a70e36b9cbdae7b5d5be6124ee6a7afc40c3b45
Luca Stefani <luca.stefani.ge1@gmail.com>
2015-12-24 22:27:36 +0000

    scripts/asus2unix.sh: For loop

--- asus2unix.sh
+++ asus2unix.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-FILE="$1"
+for FILE in $*; do
+    # Dos2unix
+    dos2unix "$FILE"
 
-dos2unix "$FILE"
+    # Tabs -> spaces
+    expand -t 4 "$FILE" > "_$FILE"
+    mv "_$FILE" "$FILE"
 
-# Tabs -> spaces
-expand -t4 "$FILE" > "_$FILE"
-mv "_$FILE" "$FILE"
+    # Remove blank lines at the end of file
+    sed -i -e :a -e '/^\n*$/{$d;N;};/\n$/ba' $FILE
 
-# Remove blank lines at the end of file
-sed -i -e :a -e '/^\n*$/{$d;N;};/\n$/ba' $FILE
-
-# Remove whitespaces at the end of lines
-sed -i 's/[[:blank:]]*$//' "$FILE"
+    # Remove whitespaces at the end of lines
+    sed -i 's/[[:blank:]]*$//' "$FILE"
+done
